92d5c83ce0e40201087ab712355b2cbc4be82c16
seokscirbe <seokscribe1970@gmail.com>
2023-04-25 01:26:00 +0900

    User 엔티티 생성

--- src/main/java/com/hanghae/myblog/entity/User.java
+++ src/main/java/com/hanghae/myblog/entity/User.java
@@ -1,2 +1,26 @@
-package com.hanghae.myblog.entity;public class User {
+package com.hanghae.myblog.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+
+
+@Entity(name = "users")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends Timestamped{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+
+    private User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+    public static User of(String username,String password){
+        return new User(username,password);
+    }
 }
+
