9ea5471e0e55c2152090f5d37189b61ab5aaa358
seokscirbe <seokscribe1970@gmail.com>
2023-04-25 16:16:57 +0900

    EnableWebSecurity 어노테이션 추가

--- src/main/java/com/hanghae/myblog/MyblogApplication.java
+++ src/main/java/com/hanghae/myblog/MyblogApplication.java
@@ -3,9 +3,11 @@ package com.hanghae.myblog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableWebSecurity
 public class MyblogApplication {
 
     public static void main(String[] args) {
