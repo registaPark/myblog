3cc20ffa422c71eb390771f6ab14b07653002999
seokscirbe <seokscribe1970@gmail.com>
2023-04-25 22:07:37 +0900

    CommentRepository수정

---
+++ src/main/java/com/hanghae/myblog/repository/CommentRepository.java
@@ -0,0 +1,10 @@
+package com.hanghae.myblog.repository;
+
+import com.hanghae.myblog.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface CommentRepository extends JpaRepository<Comment,Long> {
+}
