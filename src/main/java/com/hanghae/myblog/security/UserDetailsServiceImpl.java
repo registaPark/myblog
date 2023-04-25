6ff6b21cc7bdb1de1fc99447f0682004ed0e7c63
seokscirbe <seokscribe1970@gmail.com>
2023-04-25 11:38:50 +0900

    스프링 시큐리티 details 추가

--- src/main/java/com/hanghae/myblog/security/UserDetailsServiceImpl.java
+++ src/main/java/com/hanghae/myblog/security/UserDetailsServiceImpl.java
@@ -1,2 +1,12 @@
-package com.hanghae.myblog.security;public class UserDetailsServiceImpl {
+package com.hanghae.myblog.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
 }
