00b584cf41df96bcf13f394722ca7496ecdfbbae
seokscirbe <seokscribe1970@gmail.com>
2023-04-25 16:18:43 +0900

    Article 엔티티 빌버패턴 추가

--- src/main/java/com/hanghae/myblog/entity/Article.java
+++ src/main/java/com/hanghae/myblog/entity/Article.java
@@ -1,2 +1,29 @@
-package com.hanghae.myblog.entity;public class Article {
+package com.hanghae.myblog.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String content;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Builder
+    public Article(String title, String content, User user) {
+        this.title = title;
+        this.content = content;
+        this.user = user;
+    }
+
 }
