293046c2372e54848ef920810ad0183193a12338
seokscirbe <seokscribe1970@gmail.com>
2023-04-25 17:22:50 +0900

    ArticleDto 생성

--- src/main/java/com/hanghae/myblog/dto/article/ArticleRequestDto.java
+++ src/main/java/com/hanghae/myblog/dto/article/ArticleRequestDto.java
@@ -1,2 +1,20 @@
-package com.hanghae.myblog.dto.article;public class ArticleRequestDto {
+package com.hanghae.myblog.dto.article;
+
+import com.hanghae.myblog.entity.Article;
+import com.hanghae.myblog.entity.User;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+
+@Getter
+public class ArticleRequestDto {
+    @NotNull(message = "제목은 공백일 수 없습니다.")
+    private String title;
+    @NotNull(message = "제목은 공백일 수 없습니다.")
+    private String content;
+
+    public static Article toEntity(ArticleRequestDto articleRequestDto, User user){
+        return Article.builder().title(articleRequestDto.getTitle())
+                .content(articleRequestDto.getContent())
+                .user(user).build();
+    }
 }
--- src/main/java/com/hanghae/myblog/dto/article/ArticleResponseDto.java
+++ src/main/java/com/hanghae/myblog/dto/article/ArticleResponseDto.java
@@ -1,2 +1,21 @@
-package com.hanghae.myblog.dto.article;public class ArticleResponseDto {
+package com.hanghae.myblog.dto.article;
+
+import com.hanghae.myblog.entity.Article;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class ArticleResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+
+    public static ArticleResponseDto from(Article article){
+        return new ArticleResponseDto(article.getId(),article.getTitle(), article.getContent());
+    }
+
 }
