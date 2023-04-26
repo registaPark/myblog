877ffc47fec058c829562039f699c96e8470b2a4
seokscirbe <seokscribe1970@gmail.com>
2023-04-26 21:37:59 +0900

    좋아요 기능 추가

--- src/main/java/com/hanghae/myblog/repository/LikesRepository.java
+++ src/main/java/com/hanghae/myblog/repository/LikesRepository.java
@@ -3,5 +3,8 @@ package com.hanghae.myblog.repository;
 import com.hanghae.myblog.entity.Likes;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface LikesRepository extends JpaRepository<Likes,Long> {
+    Optional<Likes> findByUserName(String username);
 }
--- src/main/java/com/hanghae/myblog/service/LikesService.java
+++ src/main/java/com/hanghae/myblog/service/LikesService.java
@@ -1,14 +1,54 @@
 package com.hanghae.myblog.service;
 
 import com.hanghae.myblog.dto.ResponseDto;
+import com.hanghae.myblog.entity.Article;
+import com.hanghae.myblog.entity.Comment;
+import com.hanghae.myblog.entity.Likes;
 import com.hanghae.myblog.entity.User;
+import com.hanghae.myblog.exception.NoArticleException;
+import com.hanghae.myblog.exception.NoCommentException;
+import com.hanghae.myblog.repository.ArticleRepository;
+import com.hanghae.myblog.repository.CommentRepository;
+import com.hanghae.myblog.repository.LikesRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
 public class LikesService {
+    private final LikesRepository likesRepository;
+    private final ArticleRepository articleRepository;
+    private final CommentRepository commentRepository;
     public ResponseDto likeArticle(User user, Long articleId) {
-
+        Article article = articleRepository.findById(articleId).orElseThrow(() -> new NoArticleException("존재하지 않는 게시글"));
+        Optional<Likes> likesOptional = likesRepository.findByUserName(user.getUsername());
+        if(likesOptional.isPresent()){
+            likesRepository.deleteById(likesOptional.get().getId());
+            article.decreaseLikeCount();
+            return new ResponseDto("좋아요 취소",200);
+        }
+        Likes like = Likes.builder().article(article).user(user).build();
+        article.increaseLikeCount();
+        likesRepository.save(like);
+        return new ResponseDto("좋아요 성공",200);
     }
 
     public ResponseDto likeComment(User user, Long commentId) {
+        Comment comment = commentRepository.findById(commentId).orElseThrow(() -> new NoCommentException("존재하지 않는 댓글"));
+        Optional<Likes> likesOptional = likesRepository.findByUserName(user.getUsername());
+        if(likesOptional.isPresent()){
+            likesRepository.deleteById(likesOptional.get().getId());
+            comment.decreaseLikeCount();
+            return new ResponseDto("좋아요 취소",200);
+        }
+        Likes like = Likes.builder().comment(comment).user(user).build();
+        comment.increaseLikeCount();
+        likesRepository.save(like);
+        return new ResponseDto("좋아요 성공",200);
     }
 }
